60011bb9be2bc7a3b63a411acc979d936d8acc6a
Tsung-en Hsiao <joshua841025@gmail.com>
2026-02-03 18:14:35 +0800

    fix: specify HS256 algorithm for JWT encoding

--- src/main/java/com/agentvault/service/TokenService.java
+++ src/main/java/com/agentvault/service/TokenService.java
@@ -1,6 +1,8 @@
 package com.agentvault.service;
 
 import com.agentvault.model.User;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwsHeader;
 import org.springframework.security.oauth2.jwt.JwtClaimsSet;
 import org.springframework.security.oauth2.jwt.JwtEncoder;
 import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
@@ -30,6 +32,7 @@ public class TokenService {
                 // .claim("agent_id", ...) // TODO: Add agent_id logic when Agent model is ready
                 .build();
 
-        return this.jwtEncoder.encode(JwtEncoderParameters.from(claims)).getTokenValue();
+        JwsHeader jwsHeader = JwsHeader.with(MacAlgorithm.HS256).build();
+        return this.jwtEncoder.encode(JwtEncoderParameters.from(jwsHeader, claims)).getTokenValue();
     }
 }
