47958b2755abf71a286329c1051363bb4c287bb7
Tsung-en Hsiao <joshua841025@gmail.com>
2026-02-17 15:42:27 +0800

    Use fixed

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ openApi {
 
 jib {
 	from {
-		image = "amazoncorretto:21-alpine"
+		image = "gcr.io/distroless/java21-debian12@sha256:3525cafa2114ed879d3554acbbd3ba1b388e3cd3e8833ff58713ba133b0e1173"
 	}
 	to {
 		image = "agentpassvault-backend"
